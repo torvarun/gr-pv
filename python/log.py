3a1e7af71fe9e3301d4fbd1a6eba7924d57d039b
torvarun <torvarun@gmail.com>
2018-07-27 18:27:11 +0000

    logging to file

--- python/log.py
+++ python/log.py
@@ -3,3 +3,14 @@ import logging
 # Use this logger for everything related to stdout prints.
 logging.basicConfig(level = logging.INFO)
 log = logging.getLogger()
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+file_handler = logging.FileHandler('per_vices_test.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+log.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+log.addHandler(console_handler)
